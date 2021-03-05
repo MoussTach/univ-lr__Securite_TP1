5d359fd1a5be4ab7a6d51d791d96247c990b1cfd
SCD28 <75260095+SCD28@users.noreply.github.com>
2021-03-05 18:23:41 +0100

    Update RechercheFichier.c

--- RechercheFichier.c
+++ RechercheFichier.c
@@ -57,3 +57,130 @@ int main(){
         }
     }
 }
+
+
+/*
+typedef struct Element Element;
+struct Element
+{
+    char* fichier;
+    Element *e;
+};
+
+typedef struct List List;
+struct List
+{
+    Element *p;
+};
+
+bool isInfected(char* file, List *liste)
+{
+    if (liste == NULL)
+    {
+        return false;
+    }
+
+    char cible[200];
+    sprintf(cible,"%s.old",file);
+
+    Element *actuel = liste->p;
+
+    while (actuel != NULL)
+    {
+        if (strcmp(cible, actuel->fichier) == 0)
+        {
+            return true;
+        }
+        actuel = actuel->e;
+    }
+
+    return false;
+}
+
+void printList(List *liste)
+{
+    if (liste == NULL)
+    {
+        printf("Liste Vide \n");
+    }
+    else
+    {
+        Element *actuel = liste->p;
+
+        while (actuel != NULL)
+        {
+            printf("%s\n", actuel->fichier);
+            actuel = actuel->e;
+        }
+    }
+}
+
+List *init(char * file)
+{
+    List *l = malloc(sizeof(*l));
+    Element *e = malloc(sizeof(*e));
+
+    if (l == NULL || e == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    e->fichier = file;
+    e->e = NULL;
+    l->p = e;
+
+    return l;
+}
+
+List* insert(List *l, char* file)
+{
+    if (l == NULL){
+        l = init(file);
+        return l;
+    }else{
+        Element *new = malloc(sizeof(*new));
+        if (l == NULL || new == NULL)
+        {
+            exit(EXIT_FAILURE);
+        }
+        new->fichier = file;
+
+        new->e = l->p;
+        l->p = new;
+    }
+    return l;
+}
+
+int main(){
+    DIR *dirp;
+    struct dirent *entry;
+
+    struct stat s;
+    char fichier[300];
+    dirp = opendir(PATH);
+
+    List *exec = NULL;
+    List *old = NULL;
+
+
+    while ((entry = readdir(dirp)) != NULL) {
+        sprintf(fichier,"%s%s",PATH,entry->d_name);
+
+        if (stat(fichier, &s) != -1) {
+            if ((s.st_mode & S_IXUSR) && (s.st_mode & S_IFREG)){
+                if (!strstr(entry->d_name,".old"))
+                {
+                    insert(exec, fichier);
+                }
+                else
+                {
+                    insert(old, fichier);
+                }
+            }
+        }
+    }
+    printList(exec);
+    printf("fin\n");
+    return EXIT_SUCCESS;
+}
+*/
