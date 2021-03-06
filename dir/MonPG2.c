c0615a1a308e78043ee9bf635e2376f4a3dd2035
SCD28 <75260095+SCD28@users.noreply.github.com>
2021-03-06 09:57:15 +0100

    Input : nb intégrale à utiliser

--- dir/MonPG2.c
+++ dir/MonPG2.c
@@ -12,8 +12,6 @@
 
 // flags -lpthread -lm
 
-#define N 1000
-
 static pthread_mutex_t mutex;
 
 float pi = 0.0;
@@ -30,42 +28,49 @@ void *f_thread(void* arg)
 	pthread_exit(NULL);
 }
 
-int main()
+int main(int argc, char **argv)
 {
-	pthread_t tid[N];
-	long j;
-	
-	printf("--------------------- MonPG2 ---------------------\n\n");
-   	printf("Ce programme permet d'obtenir une approximation de pi avec des intégrales.\n\n"
-           "Aucune paramètre à spécifié."
-           "\n\n");
-    	printf("--------------------------------------------------\n\n");
-	
-	if (pthread_mutex_init(&mutex, NULL) != 0)
-	{
-		fprintf(stderr, "Erreur thread mutex");
-		exit(-1);
-	}
-	
-	for (j = 0 ; j < N ; j ++)
-	{
-		if (pthread_create(&tid[j], 0, f_thread, (void*) j))
-		{
-			fprintf(stderr, "Erreur thread");
-			exit(-1);
-		}
-	}
-	
-	for (j = 0 ; j < N ; j ++)
-	{
-		if (pthread_join(tid[j], NULL) != 0)
-		{
-			fprintf(stderr, "Erreur join");
-			exit(-1);
-		}
-	}
-	
-	printf("Pi = %f\n", sqrt(pi*6));
-	
-	pthread_exit(EXIT_SUCCESS);
+    if (argc == 2 && atoi(argv[1])) {
+        int N = atoi(argv[1]);
+        pthread_t tid[N];
+        long j;
+
+        printf("--------------------- MonPG2 ---------------------\n\n");
+        printf("Ce programme permet d'obtenir une approximation de pi avec des intégrales.\n\n"
+               "Input : Nombre d'intégrale à utliser pour l'approximation (minimum 1)"
+               "\n\n");
+        printf("--------------------------------------------------\n\n");
+
+        if (pthread_mutex_init(&mutex, NULL) != 0)
+        {
+            fprintf(stderr, "Erreur thread mutex");
+            exit(-1);
+        }
+
+        for (j = 0 ; j < N ; j ++)
+        {
+            if (pthread_create(&tid[j], 0, f_thread, (void*) j))
+            {
+                fprintf(stderr, "Erreur thread");
+                exit(-1);
+            }
+        }
+
+        for (j = 0 ; j < N ; j ++)
+        {
+            if (pthread_join(tid[j], NULL) != 0)
+            {
+                fprintf(stderr, "Erreur join");
+                exit(-1);
+            }
+        }
+
+        printf("Pi = %f\n", sqrt(pi*6));
+
+        pthread_exit(EXIT_SUCCESS);
+    }
+    else {
+        fprintf(stderr,"Cette fonction necessite 1 arguments\n");
+        exit(EXIT_FAILURE);
+    }
 }
